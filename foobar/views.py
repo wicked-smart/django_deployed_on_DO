767b8830f1852715d8f7ba04bf022269ba4ae099
wicked-smart <marzaanvan@gmail.com>
2022-12-05 13:34:05 +0530

    modified some files

--- foobar/views.py
+++ foobar/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, "foobar/index.html")
+     render(request, "foobar/index.html")
 
 def greet(request, name):
     return render(request, "foobar/index.html",
