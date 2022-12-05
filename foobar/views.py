def413272c1829b406adb959e0f6d7fa432d9a36
wicked-smart <marzaanvan@gmail.com>
2022-12-05 11:52:17 +0530

    Initial Commit

---
+++ foobar/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request, "foobar/index.html")
+
+def greet(request, name):
+    return render(request, "foobar/index.html",
+        {
+            "name": name
+        }
+    )
+
+
